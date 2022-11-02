1fd43308ce465ad8abbdeeae9481c586e219ed30
adindozo <adin.dozo1@outlook.com>
2022-11-02 19:22:05 +0100

    generating passwords using form with additional chekbox controls

--- routes/sifra.js
+++ routes/sifra.js
@@ -4,6 +4,7 @@ let photosJson = require('../photos.json');
 router.set("view engine" , "ejs");
 //Kreirati rutu /sifra/:k/:len koja vraća k lozinki dužine len, te ih prikazuje u tabeli.
 let generatePassword = require('password-generator')
+let generator = require('generate-password');
 router.get('/:k/:len', function(req, res) {
     let pswrds=[];
     for(let i=0;i<req.params.k;i++){
@@ -14,8 +15,24 @@ router.get('/:k/:len', function(req, res) {
 });
 
 router.post('/',(req,res)=>{
-    res.redirect('/sifra/'+req.body.k+"/"+req.body.n);
+    console.log(req.body)
+    if(!req.body.uppercase && !req.body.lowercase && !req.body.numbers)res.redirect('/sifra/'+req.body.k+"/"+req.body.n); else{
+        let passwords=generator.generateMultiple(req.body.k, {
+            length: req.body.n,
+            uppercase: req.body.uppercase=='on',
+            lowercase: req.body.lowercase=='on',
+            numbers: req.body.numbers=='on'
+        })
+        res.render("lab05-test02",  {passwords: passwords})
+    }
 })
+
+
+generator.generateMultiple(3, {
+	length: 10,
+	uppercase: false
+});
+
 
 // app.get('/:k/:len', function(req, res) {
 //     let pswrds=[];
